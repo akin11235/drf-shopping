c94e6ee22ff38465b6f2093127b987026924e70f
dele <doladunmoye@yahoo.com>
2023-12-07 11:48:00 -0500

    delete_purchased and mark_bulk_purchased custom actions added to ShoppingItemViewSet

--- shopping_list/models.py
+++ shopping_list/models.py
@@ -7,7 +7,7 @@ from django.db import models
 class ShoppingItem(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4)
     name = models.CharField(max_length=100)
-    purchased = models.BooleanField
+    purchased = models.BooleanField()
 
     def __str__(self):
         return f"{self.name}"
