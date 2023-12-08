520323dea4fe6fc6939e8dcd6dade10522d1db95
dele <doladunmoye@yahoo.com>
2023-12-08 01:32:11 -0500

    Multiple shopping lists

--- shopping_list/admin.py
+++ shopping_list/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from shopping_list.models import ShoppingItem
+from shopping_list.models import ShoppingItem, ShoppingList
 
 # Register your models here.
 
 admin.site.register(ShoppingItem)
+admin.site.register(ShoppingList)
